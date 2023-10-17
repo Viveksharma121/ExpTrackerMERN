c817ae0d616610cfa2be8b94cf67ebb3453a5e7c
Viveksharma121 <vivekksharma369@gmail.com>
2023-10-18 00:12:24 +0530

    p

--- Server/app.js
+++ Server/app.js
@@ -6,7 +6,7 @@ const { Transaction } = require("./db/db");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT;
+const PORT = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello world");
