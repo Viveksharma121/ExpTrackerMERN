4052deb337d043dfdc4f81071499169380e13680
Viveksharma121 <vivekksharma369@gmail.com>
2023-10-19 20:55:50 +0530

    c

--- Server/routes/Auth.js
+++ Server/routes/Auth.js
@@ -19,7 +19,7 @@ router.post("/reg", async (req, res) => {
     res.status(201).json({ message: "User created succesfully " });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "chutiya" });
+    res.status(500).json({ message: error });
   }
 });
 
