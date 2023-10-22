42973126cc86a382e7488c336bb9e8ba8f07b776
Viveksharma121 <vivekksharma369@gmail.com>
2023-10-22 22:14:40 +0530

    s

--- client/src/components/loginReg/Reg.jsx
+++ client/src/components/loginReg/Reg.jsx
@@ -78,7 +78,7 @@ function Reg() {
       </div>
       <button onClick={handleRegister}>Register</button>
       <p>
-        Already have an account? <Link to="/login">Login here</Link>
+        Already have an account? <Link to="/">Login here</Link>
       </p>
     </div>
   );
